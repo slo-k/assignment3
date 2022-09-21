f1ec9e30703e593cba3f271914a2532a2053d1e0
slo-k <slokawasthi312@gmail.com>
2022-09-22 01:09:21 +0530

    update

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+function reverse(){
+let str = "naman";
+let bag = "";
+for(let i=0;i=str.length;i++){
+bag= bag+str[i];
+}
+console.log(bag);
+}
+reverse();
---
+++ prime.js
@@ -0,0 +1,11 @@
+let num=15;
+for(let i=0;i<=num;i++){
+let factors = o;
+if(num%i==0){
+(factors??2)
+
+console.log("prime")
+}else{
+console.log("not prime")
+}
+}
